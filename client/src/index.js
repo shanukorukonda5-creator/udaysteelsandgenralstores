8d3e411b28b45d58382de3e6f04fd0b1a08d8aff
shanukorukonda5-creator <shanukorukonda5@gmail.com>
2026-05-12 18:57:52 +0530

    fix axios baseURL for production

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import App from './App';
 import './index.css';
 import './responsive.css';
+
+// Set axios base URL for production
+if (process.env.REACT_APP_API_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
