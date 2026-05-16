dc9ffac84181d3db7e3eb7b88f8ba45f55b01602
shanukorukonda5-creator <shanukorukonda5@gmail.com>
2026-05-16 15:04:42 +0530

    fix Gmail port 587 for Render

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,10 +1,15 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
